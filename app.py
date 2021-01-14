e4226421e4e2ab929ee1bbe13e5d30fe55b47f3d
devBro <urrzk1@gmail.com>
2021-01-14 11:22:29 +0100

    added all the files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/cyberpunktrip')
+def cyberpunktrip():
+    return render_template('cyberpunktrip.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
